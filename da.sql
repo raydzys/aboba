613b5f578cb507eb247d1c846c7ed7c9979973c5
ykov <99872237+raydzys@users.noreply.github.com>
2022-04-07 17:27:38 +0700

    Add files via upload

--- da.sql
+++ da.sql
@@ -1,11 +1,33 @@
-CREATE TABLE company
-(id INT(99) PRIMARY KEY AUTO_INCREMENT, Title VARCHAR(99), INN VARCHAR(99), OGRN VARCHAR(99), Director VARCHAR(99), adress VARCHAR(99));
+INSERT INTO cities (id, name_rus, name_eng) 
+VALUES ('3', 'Тарков', 'Tarkov'), ('4', 'Омск', 'Omsk'), ('5', 'Иркутск', 'Irkutsk');
+
+UPDATE users SET city = 2 WHERE id in(1, 2, 5);
+UPDATE users SET city = 4 WHERE id in(3, 4, 7);
+UPDATE users SET city = 1 WHERE id in(8);
+UPDATE users SET city = 5 WHERE id in(9);
+UPDATE users SET city = 3 WHERE id in(10,6);
+
+ALTER TABLE users ADD age INT(299) AFTER birth_day;
+
+UPDATE users SET age = 23 WHERE id in(1);
+UPDATE users SET age = 44 WHERE id in(2);
+UPDATE users SET age = 55 WHERE id in(3);
+UPDATE users SET age = 34 WHERE id in(4);
+UPDATE users SET age = 27 WHERE id in(5);
+UPDATE users SET age = 26 WHERE id in(6);
+UPDATE users SET age = 21 WHERE id in(7);
+UPDATE users SET age = 75 WHERE id in(8);
+UPDATE users SET age = 54 WHERE id in(9);
+UPDATE users SET age = 34 WHERE id in(10);
 
 
-CREATE TABLE city 
-(id INT(99) PRIMARY KEY AUTO_INCREMENT, ciry_ru VARCHAR(99), city_en VARCHAR(99));
-
-INSERT INTO cities (city_ru, city_en) VALUES 
-('Томск', 'Tomsk'), ('Москва', 'Moscow');
-
-
+SELECT AVG(age) AS average_age, MAX(age) AS stari, MIN(age) AS jung 
+FROM users WHERE city = 1;
+SELECT AVG(age) AS average_age, MAX(age) AS stari, MIN(age) AS jung  
+FROM users WHERE city = 2;
+SELECT AVG(age) AS average_age, MAX(age) AS stari, MIN(age) AS jung  
+FROM users WHERE city = 3;
+SELECT AVG(age) AS average_age, MAX(age) AS stari, MIN(age) AS jung  
+FROM users WHERE city = 4;
+SELECT AVG(age) AS average_age, MAX(age) AS stari, MIN(age) AS jung  
+FROM users WHERE city = 5;
